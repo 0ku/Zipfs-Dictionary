17dcb7f3e4fc737f55a7f3011f6ee6eb04f14106
Evan Guo <evan.jia.guo@gmail.com>
2021-09-07 19:35:38 -0400

    open text file working

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
 plt.plot([1,2,3,4,5],[1,1/2,1/3,1/4,1/5])
-plt.ylabel('some numbers')
-plt.show()
+
+f = open("textFile", "r")
+
+stringText = str(f.read())
+
+dictionary = {}
+
