1381b86c7e4187f47cec9e1aae1bf17d2869beb1
Evan Guo <evan.jia.guo@gmail.com>
2021-09-07 23:33:04 -0400

    parsing through words

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ plt.plot([1,2,3,4,5],[1,1/2,1/3,1/4,1/5])
 f = open("textFile", "r")
 
 stringText = str(f.read())
-
+wordList = list(map(str,stringText.split(' ')))
 dictionary = {}
-
 def appendWord(word):
     global dictionary
     if word not in dictionary.keys():
@@ -14,15 +13,14 @@ def appendWord(word):
     else:
         dictionary[word]+=1
 
-for currentWord in stringText:
+for currentWord in wordList:
     if currentWord.isnumeric() == False:
         appendWord(currentWord)
 
 y_axis = []
-x_axis = [x for x in range(1,len(dictionary.keys()))+1]
+x_axis = [x for x in range(1,len(dictionary.keys())+1)]
 
 print(dictionary)
-        
 plt.ylabel('some numbers')
 plt.show()
 
