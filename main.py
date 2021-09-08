0064bff1a50ae5bbdbf409434cc3128e6b390287
Evan Guo <evan.jia.guo@gmail.com>
2021-09-07 23:59:50 -0400

    can print dictionary, works

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 plt.plot([1,2,3,4,5],[1,1/2,1/3,1/4,1/5])
-
+nonAlphanumeric = ['[','^','-','.','/',']']
 f = open("textFile", "r")
 
-stringText = str(f.read())
+
+stringText = str(f.read()).upper()
+for a in nonAlphanumeric:
+    stringText.replace('a','')
 wordList = list(map(str,stringText.split(' ')))
+
 dictionary = {}
 def appendWord(word):
     global dictionary
@@ -20,7 +24,8 @@ for currentWord in wordList:
 y_axis = []
 x_axis = [x for x in range(1,len(dictionary.keys())+1)]
 
-print(dictionary)
+final = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+print(final)
 plt.ylabel('some numbers')
 plt.show()
 
