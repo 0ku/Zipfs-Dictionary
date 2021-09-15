e907fdd42201a783f995aa68c03293aa672dd358
Evan Guo <evan.jia.guo@gmail.com>
2021-09-15 01:39:07 -0400

    added comments, fixed textfile format

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 plt.plot([1,2,3,4,5],[1,1/2,1/3,1/4,1/5])
 nonAlphanumeric = ['[','^','-','.','/',']']
-f = open("textFile", "r")
+#opening textFile.txt
+f = open("textFile", "r",encoding="utf-8")
 
 
 stringText = str(f.read()).upper()
+#replacing/removing all non-alphanumeric characters
 for a in nonAlphanumeric:
-    stringText.replace('a','')
+    stringText.replace(a,'')
+
 wordList = list(map(str,stringText.split(' ')))
 
 dictionary = {}
@@ -18,12 +21,13 @@ def appendWord(word):
         dictionary[word]+=1
 
 for currentWord in wordList:
-    if currentWord.isnumeric() == False:
+    if currentWord.isnumeric() == False and currentWord not in nonAlphanumeric:
         appendWord(currentWord)
 
 y_axis = []
 x_axis = [x for x in range(1,len(dictionary.keys())+1)]
 
+#sorting the final dictionary to be ordered and used in graph later
 final = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
 print(final)
 plt.ylabel('some numbers')
